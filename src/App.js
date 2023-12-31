df282a6b739eaf8f4d19d1b31730f1695b0f15ed
FebinEapen <febinnn@gmail.com>
2023-12-31 14:24:42 +0530

    Digital Clock in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+
+//state
+const [time, setTime] = useState('')
+
+
+
+//add a 0 if the value is a one digit number
+function formatTime(val){
+  if (val < 10){
+    return '0'
+  } else {
+    return '';
+  }
 }
 
-export default App;
+
+//set an interval for clock tick function
+useEffect(() => {
+  //interval
+  const timerID = setInterval(
+    () => tick(),1000
+  )
+
+  //cleanup
+  return function cleanup(){
+    clearInterval(timerID)
+  }
+})
+
+//clock tick function
+function tick() {
+  //date variables
+  const d = new Date();
+  const h = d.getHours();
+  const m = d. getMinutes();
+  const s = d.getSeconds();
+
+//set the state to the formatted time
+setTime(formatTime(h) + h + ':' + formatTime(m) + m + ':' + formatTime(s) + s);
+}
+
+
+//return the jsx
+//insert the state
+return(
+  <div className='clock'>
+    <div className='screen'>
+      <h1 className='time'>{time}</h1>
+    </div>
+  </div>
+)
+
+}
